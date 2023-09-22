0b2cb857baab8222d92018841c0de2c6a9c2a20f
Iamneo <techadmin@iamneo.ai>
2023-09-22 09:17:05 +0000

    Backup Commit

---
+++ reactapp/src/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import World from './World';
+
+test('test_case01', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn React/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test_case02', () => {
+  render(<World/>);
+  const linkElement = screen.getByText(/This is World Functional Component/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
